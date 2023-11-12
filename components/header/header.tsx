202bb073fea3b4fc9c1359777b2e4a9feacee557
sajawalhassan <sajawalhassan.1feb@gmail.com>
2023-11-12 12:39:51 +0500

    Change favicon and fix ydration errors

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -9,18 +9,28 @@ import { useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
 import { Plus } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   const { resolvedTheme } = useTheme();
   const { handleOpen } = useModal();
 
   const router = useRouter();
+  if (!isMounted) return <div className="bg-white dark:bg-[#1A1A1B] h-[60px] animate-pulse"></div>;
 
   return (
     <div className="bg-white dark:bg-[#1A1A1B] px-10 flex items-center gap-x-2">
       <div className="flex items-center gap-x-2 cursor-pointer w-max" onClick={() => router.push("/")}>
         <Image src={RedditLogo} alt="Reddit Logo" className="h-[30px] w-[30px]" />
-        <Image src={resolvedTheme === "dark" ? RedditLogoTextDark : RedditLogoText} alt="Reddit Logo Text" className="h-[60px] w-[60px]" />
+        {/* <Image src={resolvedTheme === "dark" ? RedditLogoTextDark : RedditLogoText} alt="Reddit Logo Text" className="h-[60px] w-[60px]" /> */}
+        {resolvedTheme === "dark" && <Image src={RedditLogoTextDark} alt="Reddit Logo Text" className="h-[60px] w-[60px]" />}
+        {resolvedTheme === "light" && <Image src={RedditLogoText} alt="Reddit Logo Text" className="h-[60px] w-[60px]" />}
       </div>
       <div className="p-1.5 cursor-pointer rounded-full bg-zinc-200 dark:bg-transparent" onClick={() => handleOpen("createCommunity")}>
         <Plus />
--- components/providers/modals-provider.tsx
+++ components/providers/modals-provider.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { CreateCommunityModal } from "../modals/create-community-modal";
 
 export const ModalsProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return;
+
   return (
     <>
       <CreateCommunityModal />
